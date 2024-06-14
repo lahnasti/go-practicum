a90450a4354650cabe98f19fa7fbe618d0495295
Nastya <anastasilerm@mail.ru>
2024-06-14 13:52:45 +0300

    add hard ex

---
+++ day02/hard.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+//интерфейс, представляющий запись данных
+type Writer interface {
+	Write(data[]byte)(int, error)
+}
+
+type FileWriter struct {
+	file *os.File // предоставление файла
+}
+
+//структура для вывода данных в консоль
+type Console struct {}
+
+func newFile (name string) (*FileWriter, error) {
+	file, err := os.Create(name)
+	if err != nil {
+		return nil, err
+	}
+	return &FileWriter{file: file}, nil
+}
+
+func (fw *FileWriter) Write(data []byte)(int, error) {
+	return fw.file.Write(data)
+}
+
+func (c Console) Write(data []byte)(int, error) {
+	cons, err := fmt.Println(string(data))
+	return cons, err
+}
+
+func main() {
+	var writer Writer = Console{}
+
+	writer, err := newFile("file.txt")
+	if err != nil {
+		fmt.Println("Error creating", err)
+		return
+	}
+
+	data := "Hello, GO!"
+	_, err = writer.Write([]byte(data))
+	if err != nil {
+		fmt.Println("Error writing", err)
+		return
+	}
+	fmt.Println("Success")
+}
